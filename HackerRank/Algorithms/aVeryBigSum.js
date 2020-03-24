24f6488cbc5517989ebdb98a410a7f0af94b2d8f
Zakiyyah <48877866+zak92@users.noreply.github.com>
2020-03-24 19:26:35 +0200

    Update aVeryBigSum.js

--- HackerRank/Algorithms/aVeryBigSum.js
+++ HackerRank/Algorithms/aVeryBigSum.js
@@ -1,12 +1,15 @@
+/*
+Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+*/
+//Solution implemented in JavaScript
+
 function aVeryBigSum(ar) {
     var count = BigInt("0");
     for(var i = 0; i < ar.length; i++){
         count += BigInt(ar[i]);
     }
     return count;
-
-
-}
+  }
 
 var a;
 var b;
