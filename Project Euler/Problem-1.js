bf0d60f16f4cb902643ff18d02b944a332b072d7
Zakiyyah <48877866+zak92@users.noreply.github.com>
2020-03-24 19:23:03 +0200

    Update Problem-1.js

--- Project Euler/Problem-1.js
+++ Project Euler/Problem-1.js
@@ -1,13 +1,20 @@
+/*
+Problem: If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
+get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+//Solution implemented in JavaScript
+
+
 //n is the last value in a sequence
 //return the last number in the sequence that is divisible by a 
 function isDivisible(a, n){
 	for(var i = 0; i < a ; i++){
-		if((n-1)%a == i){
-			return (n-1)-i;
+		if((n-1)% a == i){
+			return (n-1) - i;
 		}
 	}
 }
-
 
 function sumMult(n, a1, a2){
 	var d1 = isDivisible(a1, n)
@@ -19,10 +26,10 @@ function sumMult(n, a1, a2){
 	var x2 = ((a2+d2)/2)*d2/a2;
 	var x3 = ((15+d3)/2)*d3/15;
 
-	if(n<15){
-		return x1+x2;
+	if(n < 15){
+		return x1 + x2;
 	}
-	return x1+x2-x3;
+	return x1 + x2 - x3;
 
 }
 
